c694048c107ceab3e99e06ec7a4a203d244f25a1
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-04-10 20:06:32 +0300

    Initial Commit

---
+++ RecordingWithAudioQueues/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecordingWithAudioQueues
+//
+//  Created by Panayotis Matsinopoulos on 10/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
